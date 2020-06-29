322ede6afa03b1b526a48824d5c86700e889adcf
Ghenadie Pusca <g.pusca@eyeo.com>
2020-06-29 09:28:45 +0300

    Add new library

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ABPTestPackage",
-            targets: ["ABPTestCombinePackage"]),
+            targets: ["ABPTestPackage"]),
+        .library(
+        name: "ABPTestCombinePackage",
+        targets: ["ABPTestCombinePackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
